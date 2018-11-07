63361217928162844a78900dfa65d8071831d226
abenito <abenito@clever-age.com>
2018-11-07 22:36:48 +0100

    menu de fin de partie

---
+++ workshop/EndMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abenito on 07/11/18.
+//
+
+#include "EndMenu.h"
---
+++ workshop/EndMenu.h
@@ -0,0 +1,14 @@
+//
+// Created by abenito on 07/11/18.
+//
+
+#ifndef WORKSHOP_ENDMENU_H
+#define WORKSHOP_ENDMENU_H
+
+
+class EndMenu {
+
+};
+
+
+#endif //WORKSHOP_ENDMENU_H
